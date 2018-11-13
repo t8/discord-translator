a1465027bb0cb452bbf97216aab89501965cf785
tate <tateflyer1@gmail.com>
2018-11-12 20:05:43 -0500

    updated the bot

--- bot.js
+++ bot.js
@@ -14,11 +14,31 @@ client.on("message", async message => {
     if(message.author.bot) {
         return;
     }
-    if (message.channel.id === "478677568724271108" || message.channel.id === "475725370587348992") {
+    //                          Spanish channel ID                             English channel ID
+    if (message.channel.id === "478677568724271108" || message.channel.id === "446075987680165890") {
+        let translatedText = "";
+        let detectedLanguage = "";
         translate(message.content, 'ES')
-            .then(res => message.reply(res.translation))
+            .then(res => translatedText = res.translation)
             .catch(console.error);
+        detectLanguage(message.content)
+            .then(res => detectedLanguage = res.languageName)
+            .catch(console.error);
+        postToChannel(detectedLanguage, translatedText);
     }
 });
+
+function postToChannel(detectedLang, tMsg) {
+    let toType = "undef";
+    if (detectedLang === "ES") {
+        // Language is english, so translate and post to spanish
+        toType = "en";
+        client.channels.get("478677568724271108").send(tMsg);
+    } else if (detectedLang === "EN") {
+        // Language is spanish, so translate and post to english
+        toType = "es";
+        client.channels.get("478677568724271108").send(tMsg);
+    }
+}
 
 client.login(process.env.DISCORD_TOKEN);
